cde99c87cfc978d957f9ea56739f5437a13e1f9a
Stefan Stegmüller <Stefan.Steg@gmail.com>
2016-04-04 17:05:42 +0200

    -add: AppBootstrapper; MainViewModel

---
+++ PicSim/AppBootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+using PicSim.ViewModels;
+
+namespace PicSim {
+  class AppBootstrapper : BootstrapperBase {
+    public AppBootstrapper() {
+      Initialize();
+    }
+
+    protected override void OnStartup(object sender, StartupEventArgs e) {
+      DisplayRootViewFor<MainViewModel>();
+    }
+  }
+}
--- PicSim/Views/MainView.xaml.cs
+++ PicSim/Views/MainView.xaml.cs
@@ -10,15 +10,14 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PicSim {
+namespace PicSim.Views {
   /// <summary>
-  /// Interaction logic for MainWindow.xaml
+  /// Interaction logic for MainView.xaml
   /// </summary>
-  public partial class MainWindow : Window {
-    public MainWindow() {
+  public partial class MainView : Window {
+    public MainView() {
       InitializeComponent();
     }
   }
