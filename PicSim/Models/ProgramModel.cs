093e8963d14c20622418139edf7a534fd7369467
Stefan Stegmüller <Stefan.Steg@gmail.com>
2016-04-04 18:48:19 +0200

    -add: ProgramModel

---
+++ PicSim/Models/ProgramModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicSim.Models {
+  class ProgramModel {
+
+    #region Fields
+
+    private Dictionary<int, int> _opcodes;
+
+    #endregion //Fields
+
+    #region Properties
+
+    #endregion //Properties
+
+    #region Constructors
+
+    public ProgramModel(string filePath) {
+      int counter = 0;
+      string line;
+
+      // Read the file and display it line by line.
+      System.IO.StreamReader file = new System.IO.StreamReader(filePath);
+      while ((line = file.ReadLine()) != null) {
+        char[] chars = line.ToCharArray();
+        if (char.IsNumber(chars[0])) {
+          System.Console.WriteLine(line);
+          _opcodes.Add(GetOPCodeIndex(line), GetOPCodeCommand(line));
+        }
+        counter++;
+      }
+
+      file.Close();
+    }
+
+    #endregion //Constructors
+
+    #region Methods
+
+    private int GetOPCodeIndex(string line) {
+      string rawIndex = line.Take<char>(4).ToString();
+      int index = Convert.ToInt32(rawIndex);
+      return index;
+    }
+
+    private int GetOPCodeCommand(string line) {
+      int command;
+      char[] commandDigits = new char[4];
+      for (int character = 5; character < 9; character++) {
+        commandDigits[character - 5] = line[character];
+      }
+      command = Convert.ToInt32(commandDigits.ToString());
+      return command;
+    }
+
+    #endregion //Methods
+
+  }
+}
--- PicSim/ViewModels/MainViewModel.cs
+++ PicSim/ViewModels/MainViewModel.cs
@@ -3,9 +3,85 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
+using PicSim.Models;
 
 namespace PicSim.ViewModels {
-  class MainViewModel {
-     
+  class MainViewModel : Caliburn.Micro.Screen {
+
+    #region Fields
+
+    private string _openFileContent;
+    private string _fileNameContent;
+
+    #endregion //Fields
+
+    #region Properties
+
+    public string OpenFileContent
+    {
+      get
+      {
+        return _openFileContent;
+      }
+
+      set
+      {
+        _openFileContent = value;
+        NotifyOfPropertyChange(() => OpenFileContent);
+      }
+    }
+
+    public string FileNameContent
+    {
+      get
+      {
+        return _fileNameContent;
+      }
+
+      set
+      {
+        _fileNameContent = value;
+        NotifyOfPropertyChange(() => FileNameContent);
+      }
+    }
+
+    #endregion //Properties
+
+    #region Constructors
+
+    public MainViewModel() {
+      OpenFileContent = "Open File";
+    }
+
+    #endregion //Constructors
+
+    #region Methods
+
+    public void OpenFile() {
+      // Create OpenFileDialog 
+      Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+
+
+      // Set filter for file extension and default file extension 
+      dlg.DefaultExt = ".lst";
+      dlg.Filter = "LST Files (*.lst)|*.lst";
+
+
+      // Display OpenFileDialog by calling ShowDialog method 
+      Nullable<bool> result = dlg.ShowDialog();
+
+
+      // Get the selected file name and display in a TextBox 
+      if (result == true) {
+        // Open document 
+        FileNameContent = dlg.SafeFileName;
+        ProgramModel progModel = new ProgramModel(dlg.FileName);
+      }      
+    }
+
+    #endregion //Methods
+
   }
 }
