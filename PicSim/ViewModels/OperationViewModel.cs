c3af749594c5ca03a82e559811c48d3c24b88403
Stefan Stegmüller <Stefan.Steg@gmail.com>
2016-04-06 15:22:11 +0200

    -add: OperationView and OperationViewModel

---
+++ PicSim/ViewModels/OperationViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace PicSim.ViewModels {
+  class OperationViewModel : PropertyChangedBase {
+
+    #region Fields
+
+    private bool _breakPoint;
+    private string _operationName;
+    private string _operationArgs;
+
+    #endregion //Fields
+
+    #region Properties
+
+    public bool BreakPoint
+    {
+      get
+      {
+        return _breakPoint;
+      }
+
+      set
+      {
+        _breakPoint = value;
+        NotifyOfPropertyChange(() => BreakPoint);
+      }
+    }
+
+    public string OperationName
+    {
+      get
+      {
+        return _operationName;
+      }
+
+      set
+      {
+        _operationName = value;
+        NotifyOfPropertyChange(() => OperationName);
+      }
+    }
+
+    public string OperationArgs
+    {
+      get
+      {
+        return _operationArgs;
+      }
+
+      set
+      {
+        _operationArgs = value;
+        NotifyOfPropertyChange(() => OperationArgs);
+      }
+    }
+
+    #endregion //Properties
+
+    #region Constructors
+
+    public OperationViewModel(string opName, string args) {
+      OperationName = opName;
+      OperationArgs = args;
+    }
+
+    #endregion //Constructors
+
+    #region Methods
+
+    #endregion //Methods
+
+  }
+}
