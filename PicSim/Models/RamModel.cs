96315db2d46fa137d37ec49a94bf1038ae75e83a
Stefan Stegmüller <Stefan.Steg@gmail.com>
2016-04-14 18:32:49 +0200

    -add: functions for ramModel

--- PicSim/Models/RamModel.cs
+++ PicSim/Models/RamModel.cs
@@ -6,5 +6,52 @@ using System.Threading.Tasks;
 
 namespace PicSim.Models {
   class RamModel {
-  }
+
+		#region Fields
+
+		private byte[] _ram;
+
+		#endregion //Fields
+
+		#region Properties
+		public byte[] Ram {
+			get {
+				return _ram;
+			}
+
+			set {
+				_ram = value;
+			}
+		}
+
+		#endregion //Properties
+
+		#region Constructors
+
+		public RamModel() {
+			Ram = new byte[0xFF];
+		}
+
+		#endregion //Constructors
+
+		#region Methods
+
+		public void SetRegisterValue(int adress, int value) {
+			Ram[adress] = Convert.ToByte(value);
+		}
+
+		public int GetRegisterValue(int adress) {
+			return Convert.ToInt32(Ram[adress]);
+		}
+
+		public void ToggleRegisterBit(int adress, int bit, bool set) {
+			if (set && bit < 8 && bit > -1) {
+				byte mask = Convert.ToByte(0x01 << bit);
+				byte value = Ram[adress];
+				Ram[adress] = (byte)(value | mask);
+			}			
+		}
+
+		#endregion //Methods
+	}
 }
---
+++ PicSim/SpecialFunctionRegister.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicSim {
+	enum SpecialFunctionRegister {
+		INDF				= 0x00,
+		TMR0				= 0x01,
+		PCL					= 0x02,
+		STATUS			= 0x03,
+		FSR					= 0x04,
+		PORTA				= 0x05,
+		PORTB				= 0x06,
+		EEDATA			= 0x08,
+		EEADR				= 0x09,
+		PCLATH			= 0x0A,
+		INTCON			= 0x0B,
+
+		OPTION_REG	= 0x81,
+		TRISA				= 0x85,
+		TRISB				= 0x86,
+		EECON1			= 0x88,
+		EECON2			= 0x89,
+	}
+}
