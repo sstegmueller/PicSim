2fe62abba159e00e1e6244d08d5c65ea5bbb651f
simon.hofmann <simon.hofmann@SIHOE5440>
2016-05-22 21:23:51 +0200

    -add: open pdf-file with relative path

--- PicSim/ViewModels/HelpViewModel.cs
+++ PicSim/ViewModels/HelpViewModel.cs
@@ -3,8 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
 
 namespace PicSim.ViewModels {
-  class HelpViewModel {
-  }
+  class HelpViewModel : PropertyChangedBase {
+        public void OpenPDFFile()
+        {     
+                 
+            System.Diagnostics.Process.Start("..\\..\\Doku.pdf");
+                       
+        }
+    }
 }
